674d6f811fd249dca92aa1e2c4fd8ae4c1dc2236
Yves Ruosch <yves@ruosch.me>
2021-02-14 11:12:25 +0100

    Step 137

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+
+    [SerializeField] float maxSpwanDelay = 1f;
+    [SerializeField] float minSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+
+    IEnumerator Start()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpwanDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+    }
+
+
+}
