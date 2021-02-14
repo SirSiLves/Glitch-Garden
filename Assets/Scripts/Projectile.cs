634ad48112823e562bf76a8fe58e517f3dcb0871
Yves Ruosch <yves@ruosch.me>
2021-02-14 13:36:51 +0100

    Step 144

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
 
-    [SerializeField] float speed = 1f;
+    [SerializeField] float speed = 5f;
     [SerializeField] float damage = 50f;
 
 
