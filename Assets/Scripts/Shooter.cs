1ca45cc5d05a53b419baf7ee280ce946dad70b70
Yves Ruosch <yves@ruosch.me>
2021-02-14 18:28:20 +0100

    Step 152

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,41 @@ public class Shooter : MonoBehaviour
 {
 
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
 
+    void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            print("SHOOT!!!");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner newAttacker in spawners)
+        {
+
+            bool isCloseEnough = (Mathf.Abs(newAttacker.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+            {
+                myLaneSpawner = newAttacker;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        return myLaneSpawner.transform.childCount > 0;
+    }
 
     public void Fire()
     {
