dafed264adfe77d8dc327b2df66402dc005eae36
Yves Ruosch <yves@ruosch.me>
2021-02-14 11:57:01 +0100

    Step 139

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,25 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50f;
+
 
     void Update()
     {
         transform.Translate(Vector2.right * Time.deltaTime * speed);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherColider)
+    {
+        var health = otherColider.GetComponent<Health>();
+        var attacker = otherColider.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+
+
 }
